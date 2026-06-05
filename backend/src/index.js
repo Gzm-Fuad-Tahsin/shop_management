7701762d8c822a0080caed910907417217eb35ea
Gzm Fuad Tahsin <73604797+Gzm-Fuad-Tahsin@users.noreply.github.com>
2026-06-05 16:32:15 +0600

    fd

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ const app = express()
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://shop-management-zapo.onrender.com","https://shop-management-kappa.vercel.app","https://vendastra-frontend.vercel.app/"],
+    origin: ["http://localhost:3000", "https://shop-management-zapo.onrender.com","https://shop-management-kappa.vercel.app","https://vendastra-frontend.vercel.app"],
     credentials: true,
   }),
 )
