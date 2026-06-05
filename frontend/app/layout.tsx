f625b4a454cf2efaeee61ccd8efa733eab211846
Gzm Fuad Tahsin <73604797+Gzm-Fuad-Tahsin@users.noreply.github.com>
2026-06-05 15:07:20 +0600

    s

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from 'react'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
 import { ServiceWorkerCleanup } from '@/components/sw-unregister'
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' })
